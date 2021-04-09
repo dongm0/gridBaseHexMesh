619bcc467aaf4878d6ca68ee6a72f3b88e436e68
dongmo <dong_mo@163.com>
2021-04-09 21:28:30 +0800

    20210408 save

--- src/simplemesh.h
+++ src/simplemesh.h
@@ -2,17 +2,82 @@
 
 #include <fstream>
 #include <iostream>
+#include <sstream>
 #include <string>
 
 struct SimpleMesh {
 public:
+  enum class MeshType {
+    Tri,
+    Quad,
+    Tet,
+    Hex
+  };    
+
   Eigen::MatrixXd V;
   Eigen::MatrixXi C;
 
   SimpleMesh();
   SimpleMesh(const SimpleMesh &rhs);
   SimpleMesh(const Eigen::MatrixXd &V, const Eigen::MatrixXi &C);
-  SimpleMesh(const std::string &filename);
+  SimpleMesh(const std::string &filename) {}
+
+  void InputVtk(std::istream &stream) {
+    std::stringstream ss;
+    std::string line;
+    std::string stmp;
+    uint32_t vnum = 0, cnum = 0;
+    std::getline(stream, line);
+    std::getline(stream, line);
+    std::getline(stream, line);
+    if (line != "ASCII") {
+      throw std::runtime_error("ascii only!");
+    }
+    std::getline(stream, line);
+    std::getline(stream, line);
+    ss.clear();
+    ss.str(line);
+    ss >> stmp >> vnum >> stmp;
+    V.resize(vnum, 3);
+
+    std::vector<double> v;
+    for (uint32_t i = 0; i < vnum; ++i) {
+      std::getline(stream, line);
+      ss.clear();
+      ss.str(line);
+      ss >> v[0] >> v[1] >> v[2];
+      V.row(i) << v[0], v[1], v[2];
+    }
+
+    std::getline(stream, line);
+    ss.clear();
+    ss.str(line);
+    ss >> stmp >> cnum >> stmp;
+    C.resize(cnum, 3);
+    uint32_t _r[3];
+
+    for (uint32_t i = 0; i < cnum; ++i) {
+      std::getline(stream, line);
+      ss.clear();
+      ss.str(line);
+      ss >> stmp >> _r[0] >> _r[1] >> _r[2];
+      C.row(i) << _r[0], _r[1], _r[2];
+    }
+
+    std::getline(stream, line);
+    ss.clear();
+    ss.str(line);
+    for (uint32_t i=0; i<cnum; ++i) {
+      int itmp;
+      std::getline(stream, line);
+      ss.clear();
+      ss.str(line);
+      ss >> itmp;
+      if (itmp!=5) {
+        throw std::runtime_error("triangle only!");
+      }
+    }
+  }
 
   void OutputVtk(std::ostream &stream) {
     stream << "# vtk DataFile Version 3.0\ngenerate by dongmo\nASCII\nDATASET "
--- src/test.cpp
+++ src/test.cpp
@@ -2,6 +2,17 @@
 #include "simplemesh.h"
 #include "gridBaseHexMesh.h"
 
+#include <fstream>
+
 int main() {
-    
+    SimpleMesh mesh, meshout;
+    std::ifstream fin;
+    fin.open("test.vtk");
+    mesh.InputVtk(fin);
+    fin.close();
+    std::ofstream fout;
+    fout.open("out.vtk");
+    GridBaseHexMesh::Voxel(mesh, meshout, 100);
+    meshout.OutputVtk(fout);
+    fout.close();
 }
